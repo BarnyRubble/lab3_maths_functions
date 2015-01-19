99ffab02ac6e0f01a5acc8c01395dbf684fa2a08
Barnaby Stewart <p14167376@myemail.dmu.ac.uk>
2015-01-19 13:21:00 +0000

    Added maths_functions.c

---
+++ maths_functions.c
@@ -0,0 +1,27 @@
+// FILE: maths_functions.c
+
+int add_values (int n1, int n2)
+{
+	return n1 + n2;
+}
+
+int subtract_values (int n1, int n2)
+{
+	return n1 - n2;
+}
+
+int multiply_values (int n1, int n2)
+{
+	return n1 * n2;
+}
+
+int divide_values (int n1, int n2)
+{
+	return n1 / n2;
+}
+
+int mod_values (int n1, int n2)
+{
+	return mod (n1, n2);
+}
+
